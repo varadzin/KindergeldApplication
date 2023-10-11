d47bb10ae179dce7f600b2bef9bb6e5de5421b3f
Frantisek Varadzin <varadzin@adesso-mobile.de>
2023-10-11 22:17:25 +0200

    master: Print View Buttons

--- KindergeldApplication/Views/PrintView.swift
+++ KindergeldApplication/Views/PrintView.swift
@@ -11,20 +11,76 @@ struct PrintView: View {
     var body: some View {
         NavigationView {
             VStack {
-                Text("Pre žiadosť o Kindergeld potrebuješ nasledovné dokumenty:")
-                Text("1. Hlavná žiadosť o prídavky")
-                    .padding(.vertical, 20)
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.red)
-                Text("Antrag auf Kindergeld KG 1-cz")
-                    .font(.caption2)
-                Spacer()
+                Text("Pre žiadosť o Kindergeld potrebuješ dokumenty:")
+                    .padding(.horizontal, 10)
+                    .frame(idealWidth: 300)
+                VStack(alignment: .leading) {
+                    Text("1. Hlavná žiadosť o prídavky")
+                        .padding(.vertical, 10)
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.red)
+                    Text("Antrag auf Kindergeld KG 1-cz")
+                        .font(.caption2)
+                        .padding(.leading, 10)
+                    HStack {
+                        Image(systemName: "doc.text.fill")
+                            .foregroundColor(.red)
+                            .font(.largeTitle)
+                        Text("Vyplnene na 75%")
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    HStack {
+                        docButton(buttonImage: "magnifyingglass", buttonText: "prezrieť dokument")
+                        docButton(buttonImage: "paperplane.fill", buttonText: "poslať dokument")
+                        
+                        docButton(buttonImage: "printer.fill", buttonText: "tlačiť dokument")
+                        
+                        
+                    }
+                    Spacer(minLength: 200)
+                }
+                .padding(.horizontal, 10)
             }
+            
             .navigationTitle("Hotové dokumenty")
         }
     }
 }
+
+extension PrintView {
+    func docButton(buttonImage: String, buttonText: String) -> some View {
+        Button {
+            print("Btn tapped")
+        } label: {
+            VStack {
+                Image(systemName: buttonImage)
+                    .font(.largeTitle)
+                    .padding(4)
+                Text(buttonText)
+                    .font(.caption)
+                    .padding(.horizontal, 5)
+            }
+        }
+        .frame(width: 100, height: 100)
+       .foregroundColor(.systemGray)
+        .background(Color.systemGray6)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(radius: 4)
+        .padding(10)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 struct PrintView_Previews: PreviewProvider {
     static var previews: some View {
