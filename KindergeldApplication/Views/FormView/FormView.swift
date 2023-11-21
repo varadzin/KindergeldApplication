3dcfbf7efb0d9520a26f2d2e2c4650b55064cebf
Frantisek Varadzin <varadzin@adesso-mobile.de>
2023-11-21 19:36:09 +0100

    master: create Form View

--- KindergeldApplication/Views/FormView/FormView.swift
+++ KindergeldApplication/Views/FormView/FormView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct FormView: View {
+    
+    @State var manTitle: String = ""
+    @State var manFirstname: String = ""
+    @State var manSurename: String = ""
+    
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack {
-                    TopStepBarView(step: 1)
-                        .padding(.vertical, 8)
-                
+                    topStepBar
+                    VStack(alignment: .leading) {
+                        title
+                        explanationText
+                        subtitle
+                        formTextFields
+                    }
+                    
                 }
+                .padding(.horizontal, 15)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
@@ -23,7 +34,7 @@ struct FormView: View {
                 }
             }
         }
-      
+        
     }
 }
 
@@ -33,6 +44,63 @@ extension FormView {
             print("Language Button tapped")
         } label: {
             LanguageButton()
+        }
+    }
+    
+    var topStepBar: some View {
+        TopStepBarView(step: 1)
+            .padding(.vertical, 8)
+    }
+    
+    
+    var title: some View {
+        Text("Formuláre")
+            .font(.largeTitle)
+            .fontWeight(.bold)
+            .padding()
+    }
+    
+    var explanationText: some View {
+        Text("Pre žiadosť o Kindergeld potrebuješ vyplniť nasledovné dokumenty. Ak je niečo nezrozumteľné, tak klikni vpravo na Vysvetlivky \(Image(systemName: "questionmark.circle"))")
+            .font(.title3)
+    }
+    var subtitle: some View {
+        VStack {
+            Text("Údaje žiadateľa")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(Color.customRed)
+            Text("Pozri vysvetlivky, kto môže žiadať o prídavky")
+                .font(.caption2)
+                .foregroundStyle(Color.label)
+        }
+        .padding(.vertical, 15)
+    }
+    
+    var formTextFields: some View {
+        VStack {
+            HStack {
+                TextField("Titul", text: $manTitle)
+                    .textFieldStyle(CustomTextField())
+                Button {
+                  print("Title Button tapped")
+                } label: {
+                    Image(systemName: "questionmark.circle")
+                        .font(.largeTitle)
+                        .foregroundColor(.customRed)
+                }
+            }
+            HStack {
+            TextField("Meno", text: $manFirstname)
+                .textFieldStyle(CustomTextField())
+                Button {
+                   print("Name Button tapped")
+                } label: {
+                    Image(systemName: "questionmark.circle")
+                        .font(.largeTitle)
+                        .foregroundColor(.customRed)
+                }
+                }
         }
     }
 }
